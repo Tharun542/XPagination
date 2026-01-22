b61bcf061a85380cf06e67ac149c814c506736b3
tharun <godasutharun143@gmail.com>
2026-01-22 14:58:00 +0530

    pagination developed

---
+++ src/Pagination.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "./pagination.css";
+
+export default function Pagination(){
+    const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const rowsPerPages = 10;
+    const totalPages = Math.ceil(data.length / rowsPerPages);
+
+    const startIndex = (currentPage - 1) * rowsPerPages;
+    const currentRows = data.slice(startIndex, startIndex + rowsPerPages)
+
+    useEffect(()=>{
+        const url =async()=>{
+            const res = await fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json")
+            const upiPage = await res.json()
+            setData(upiPage);
+        }
+        url();
+    },[])
+
+
+    return(
+        <div className="tbl">
+            <h1>Empolyee Data Table</h1>
+            <div className="tbl1">
+                <table>
+                    <thead>
+                        <tr>
+                        <th>id</th>
+                        <th >Name</th>
+                        <th >Email</th>
+                        <th >Role</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {currentRows.map((item)=>(
+                            <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.email}</td>
+                            <td>{item.role}</td>
+                            </tr>
+                        ))}
+                    </tbody>    
+                </table>
+            </div>
+           <br />
+           <div className="div">
+            <button className="btn" onClick={()=>setCurrentPage(prev => prev - 1)} disabled={currentPage===1}>Previous</button>
+           <button className="btn">{currentPage}</button>
+            <button className="btn" onClick={()=>setCurrentPage(prev => prev + 1)} disabled={currentPage===totalPages}>Next</button>
+           </div>
+            
+        </div>
+    )
+}
